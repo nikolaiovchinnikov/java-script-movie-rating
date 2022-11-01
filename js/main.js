96a6ed08d240307405374414524e923eb55b8a9e
Ovchinnikov Nikolay <slavanikolu.gmail.com>
2022-11-01 21:14:06 +0600

    добавил цикл и функцию поиска и проверки на коректность ввода

--- js/main.js
+++ js/main.js
@@ -1,2 +1,52 @@
 "use strict";
-console.log();
+let moveName = "yes";
+const movies = [
+    {
+        'name': 'Interstellar',
+        'ratings': {
+            'John': 10,
+            'Jack': 3
+        }
+    },
+    {
+        'name': 'Avengers: Infinity War',
+        'ratings': {
+            'Jack': 9,
+            'Jane': 10
+        }
+    }
+];
+const addObject = () => {
+};
+const deleteObject = () => {
+};
+const listObject = () => {
+};
+const rateObject = () => {
+};
+const findObject = (moveName) => {
+    for (let i = 0; i < movies.length; i++) {
+        if (movies[i]['name'] === moveName) {
+            return `Найден фильм ${movies[i]["name"]}`;
+        }
+    }
+    return "такого фильма нет в списке";
+};
+const userReuuest = () => {
+    while (true) {
+        const moveName = prompt("Введите название фильма");
+        if (moveName === null) {
+            alert("Вы завершили цикл");
+            return "";
+        }
+        else if (moveName === "") {
+            alert("Повторите ввод");
+            continue;
+        }
+        return moveName;
+    }
+};
+while (moveName) {
+    moveName = userReuuest();
+    console.log(findObject(moveName));
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,59 @@
-console.log()
+
+let moveName:  string = "yes"
+
+const movies = [
+    {
+        'name': 'Interstellar',
+        'ratings': {
+            'John': 10,
+            'Jack': 3
+        }
+    },
+    {
+        'name': 'Avengers: Infinity War',
+        'ratings': {
+            'Jack': 9,
+            'Jane': 10
+        }
+    }
+]
+const addObject = () => {
+
+}
+const deleteObject = () => {
+
+}
+const listObject = () => {
+
+}
+const rateObject = () => {
+
+}
+const findObject = (moveName: string): string => {
+    for (let i = 0; i < movies.length; i++) {
+        if(movies[i]['name'] === moveName){
+            return `Найден фильм ${movies[i]["name"]}`
+        }
+    }
+    return "такого фильма нет в списке"
+}
+const userReuuest = (): string => {
+    while(true){
+        const moveName = prompt("Введите название фильма");
+        if (moveName === null){
+            alert("Вы завершили цикл");
+            return "";
+        }else if(moveName === ""){
+            alert("Повторите ввод");
+            continue;
+        }return moveName;
+    }
+}
+
+
+
+
+while (moveName){
+    moveName = userReuuest();
+     console.log(findObject(moveName));
+}
